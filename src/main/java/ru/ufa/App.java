a92877a2c6ccbe481837e512d6f294ca59c43548
Tagir Almukhametov <a.tagir@list.ru>
2022-06-04 13:14:55 +0500

    homework 6.1 param v1

---
+++ src/main/java/ru/ufa/App.java
@@ -0,0 +1,39 @@
+package ru.ufa;
+
+import java.util.Scanner;
+
+class Main {
+
+    public static final double PI = 3.14;
+
+    public static void main(String[] args) {
+//        System.out.println("Программа расчета площади круга и длины окружности");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите радиус окружности в сантиметрах:");
+//        int radius = scanner.nextInt();
+        int radius = 10;
+        int smallRadius = 5;
+        int ringArea = (int) calculateRingArea(smallRadius, radius);
+        double area = calculateCircleArea(radius);
+        double length = calculateCircleLength(radius);
+        System.out.println("Целая часть площади кольца: " + ringArea + " см.кв.");
+        System.out.println("Площадь круга: " + area + " см.кв.");
+        System.out.println("Длина окружности: " + length + " см.");
+    }
+
+    public static double calculateCircleArea(int radius) {
+        return PI * radius * radius;
+    }
+
+    public static double calculateCircleLength(int radius) {
+        return 2 * PI * radius;
+    }
+
+    public static int calculateRingArea(int smallRadius, int bigRadius) {
+        return (int) ((PI * bigRadius * bigRadius) - (PI * smallRadius * smallRadius));
+    }
+
+    public static String theCapitalOfGreatBritian() {
+        return "London";
+    }
+}
---
+++ src/test/java/ru/ufa/AppTest.java
@@ -0,0 +1,71 @@
+package ru.ufa;
+
+import static org.junit.Assert.assertTrue;
+import static ru.ufa.Main.calculateRingArea;
+import static ru.ufa.Main.theCapitalOfGreatBritian;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class AppTest {
+    private static long suiteStartTime;
+    public static final double PI = 3.14;
+    private long testStartTime;
+
+    @BeforeAll
+    public static void initSuite() {
+        System.out.println("Running All Tests");
+        suiteStartTime = System.nanoTime();
+    }
+
+    @AfterAll
+    public static void completeSuite() {
+        System.out.println("All Tests completed in time: " + (System.nanoTime() - suiteStartTime));
+    }
+
+    @BeforeEach
+    public void initTest() {
+        System.out.println("Starting new test");
+        testStartTime = System.nanoTime();
+    }
+
+    @AfterEach
+    public void finalizeTest() {
+        System.out.println("Test complete in time: " + (System.nanoTime() - testStartTime));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(theCapitalOfGreatBritian().equals("London"));
+    }
+
+//    @org.junit.jupiter.api.Test
+//    public void testConcat_validArgument_success() {
+//        // given:
+//  ...
+//
+//        // when:
+//  ...
+//
+//        // then:
+//        Assertions.***;
+//    }
+
+    @ParameterizedTest
+    @MethodSource("methodSource")
+    public void testParamMethodSourcecalculateRingArea(int a, int b, int expectedResult) {
+        int result = calculateRingArea(a, b);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    public Stream<Arguments> methodSource() {
+        return Stream.of(
+                Arguments.of(10, 5, 335),
+                Arguments.of(7, 4, 105)
+        );
+    }
+}
